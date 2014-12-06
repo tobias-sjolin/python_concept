924178b807c327e400fc47a43e9e8518b154e62b
tsj <tsj@boozt.com>
2014-12-06 21:49:12 +0100

    Added message function

--- concept.py
+++ concept.py
@@ -17,6 +17,28 @@ if rv == 'OK':
     print "Mailboxes:"
     print mailboxes
 
+def process_mailbox(M):
+    rv, data = M.search(None, "ALL")
+        if rv != 'OK':
+            print "No messages found!"
+            return
+                    
+        for num in data[0].split():
+            rv, data = M.fetch(num, '(RFC822)')
+            if rv != 'OK':
+                print "ERROR getting message", num
+                return
+                                        
+            msg = email.message_from_string(data[0][1])
+            print 'Message %s: %s' % (num, msg['Subject'])
+            print 'Raw Date:', msg['Date']
+            date_tuple = email.utils.parsedate_tz(msg['Date'])
+            if date_tuple:
+                local_date = datetime.datetime.fromtimestamp(
+                    email.utils.mktime_tz(date_tuple))
+                print "Local Date:", \
+                    local_date.strftime("%a, %d %b %Y %H:%M:%S")
+
 rv, data = M.select("Top Secret/PRISM Documents")
 if rv == 'OK':
     print "Processing mailbox...\n"
