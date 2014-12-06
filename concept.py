520d8c88aa037d8d93330ff24296c88f40142c4c
tsj <tsj@boozt.com>
2014-12-06 21:56:00 +0100

    Added message function

--- concept.py
+++ concept.py
@@ -4,33 +4,22 @@ import getpass
 import email
 import datetime
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
-
-try:
-    M.login(sys.argv[1], sys.argv[2])
-except imaplib.IMAP4.error:
-    print "LOGIN FAILED!!! "
-    # ... exit or deal with failure...
-
-rv, mailboxes = M.list()
-if rv == 'OK':
-    print "Mailboxes:"
-    print mailboxes
-
 def process_mailbox(M):
     rv, data = M.search(None, "ALL")
     if rv != 'OK':
         print "No messages found!"
         return
-                    
+
     for num in data[0].split():
         rv, data = M.fetch(num, '(RFC822)')
         if rv != 'OK':
             print "ERROR getting message", num
             return
-                                        
+    
         msg = email.message_from_string(data[0][1])
-        print 'Message %s: %s' % (num, msg['Subject'])
+        decode = email.header.decode_header(msg['Subject'])[0]
+        subject = unicode(decode[0])
+        print 'Message %s: %s' % (num, subject)
         print 'Raw Date:', msg['Date']
         date_tuple = email.utils.parsedate_tz(msg['Date'])
         if date_tuple:
@@ -39,7 +28,24 @@ def process_mailbox(M):
             print "Local Date:", \
                 local_date.strftime("%a, %d %b %Y %H:%M:%S")
 
-rv, data = M.select("Top Secret/PRISM Documents")
+
+M = imaplib.IMAP4_SSL('imap.gmail.com')
+
+try:
+    rv, data = M.login(sys.argv[1], sys.argv[2])
+except imaplib.IMAP4.error:
+    print "LOGIN FAILED!!! "
+    # ... exit or deal with failure...
+
+print rv, data
+
+rv, mailboxes = M.list()
+if rv == 'OK':
+    print "Mailboxes:"
+    print mailboxes
+
+
+rv, data = M.select("INBOX")
 if rv == 'OK':
     print "Processing mailbox...\n"
     process_mailbox(M) # ... do something with emails, see below ...
